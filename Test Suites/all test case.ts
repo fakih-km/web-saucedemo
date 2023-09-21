b9ac4cebe1e121088f2d0ced2be1eecd7180267a
fakih <fakih.kakung@gmail.com>
2023-09-21 15:29:07 +0700

    add test suites

---
+++ Test Suites/all test case.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>all test case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>db08b3f6-2684-4860-bd45-cec332af38a9</testSuiteGuid>
+   <testCaseLink>
+      <guid>c73c8aad-89a8-41ed-9ead-422f0ee13a4f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login_Failed</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9accb0b2-86b2-4cbc-9202-99330e2a6609</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login_success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c288f271-4b4a-4bf2-8e95-f3117b4530fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Shopping/Shopping success</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
